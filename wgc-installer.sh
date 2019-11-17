31284ea942ab215e5b759acd01c5a1b7403092f6
NiiWiiCamo <mail@nilsblume.de>
2019-11-17 23:24:51 +0100

    Fixed iptables Postup / Postdown

--- wgc-installer.sh
+++ wgc-installer.sh
@@ -138,8 +138,8 @@ cat << ENDINTERFACE > ${wginterface}.conf
 Address = ${wgnetwork}${wgserverip}/24
 ListenPort = ${wgserverport}
 PrivateKey = ${srvprivkey}
-PostUp = iptables -A FORWARD -i %i -j ACCEPT; iptables -A F$
-PostDown = iptables -D FORWARD -i %i -j ACCEPT; iptables -D$
+PostUp = iptables -A FORWARD -i %i -j ACCEPT; iptables -A FORWARD -o %i -j ACCEPT; iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+PostDown = iptables -D FORWARD -i %i -j ACCEPT; iptables -D FORWARD -o %i -j ACCEPT; iptables -t nat -D POSTROUTING -o eth0 -j MASQUERADE
 SaveConfig = true
 
 #############################
