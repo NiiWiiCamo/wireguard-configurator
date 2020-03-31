5e51057948f59f0db882a45911583a82c3decace
NiiWiiCamo <mail@nilsblume.de>
2020-03-31 10:45:52 +0200

    peer editor not fully working, returning object back through menu not yet possible

--- wgc.py
+++ wgc.py
@@ -3,11 +3,11 @@
 
 import os
 from pathlib import Path
+import copy
 
 operatingsystem: str = "Windows"
 distribution: str = 'unknown'
 wireguardpath: str = Path('.') / "wireguard-test" / "wireguard"
-selectedinterface: str = "wg0"
 
 
 # Main Loop. Yes.
@@ -66,7 +66,6 @@ def listfile(path: str, filefilter: str):
 
 # Getting your menu displayed. Might be useful for visualizing lists. Like Menu items. Or lists.
 def printlistmenu(lis: list):
-    i: int = 0
     for i in range(len(lis)):
         print(str(i) + ": " + str(lis[i]))
 
@@ -92,7 +91,10 @@ def makemenu(title: str, labels: list, commands: list, maxtries: int = 3):
             else:
                 function = command_set
                 args = []
-            result = function(*args)
+            if function == "returnselection":
+                return selection
+            else:
+                result = function(*args)
             if result:
                 break
         else:
@@ -103,17 +105,15 @@ def makemenu(title: str, labels: list, commands: list, maxtries: int = 3):
 def testmenu(additional_title: str = ""):
     title: str = f"Test Menu {additional_title}"
     labels: list = ["Return to previous menu", "option1", "option2"]
-    commands: list = [(returnselection,), (testmenu, "Option 1"), (print, "Option 2")]
+    commands: list = [(returnmenu,), (testmenu, "Option 1"), (print, "Option 2")]
     makemenu(title, labels, commands)
 
 
 # The. Menu.
 def mainmenu():
     title: str = "WGC Main Menu"
-    labels: list = ["Quit WGC", "Configure shared networks", "Configure P2P networks", "Filebrowser", "Testmenu 1",
-                    "Testmenu 2"]
-    commands: list = [quitgracefully, p2mpmenu, p2pmenu, (filebrowser, Path.cwd()), (testmenu, "number 1"),
-                      (testmenu, "number 2")]
+    labels: list = ["Quit WGC", "Configure shared networks", "Configure P2P networks"]
+    commands: list = [quitgracefully, p2mpmenu, p2pmenu]
     makemenu(title, labels, commands)
 
 
@@ -121,7 +121,7 @@ def mainmenu():
 def filebrowser(path: Path):
     title: str = f"Filebrowser at {path}"
     labels: list = ["Back"]
-    commands: list = [(returnselection,)]
+    commands: list = [(returnmenu,)]
     for child in path.iterdir():
         if child.is_file():
             labels.append(f"View File Contents of {child}")
@@ -149,7 +149,7 @@ def p2mpmenu():
     interfaces: list = listfile(wireguardpath, filefilter)
     title: str = "Shared Networks (P2MP Server)"
     labels: list = ["Return to Main Menu"]
-    commands: list = [returnselection]
+    commands: list = [returnmenu]
     for interface in interfaces:
         labels.append(interface)
         commands.append((modifyp2mpmenu, interface))
@@ -170,35 +170,72 @@ def createp2mp():
 
 # Something, something, I might need this later.
 def modifyp2mpmenu(interface: str):
-    conf = readconfig(interface)
-    intname: str = interface.split(".")[0].strip()
+    conf: ConfigInterface = readconfig(interface)
+    intname = conf.interface
     alias: str = conf.alias
     ipv4 = conf.ipv4
     ipv6 = conf.ipv6
     peercount: int = len(conf.peers)
-    header: str = "Editing P2MP Interface: " + intname
-    if alias != "":
-        header = header + " with Alias: " + alias
     print("")
-    print(header)
+    print("Editing P2MP Interface: " + intname)
+    if alias != "":
+        print("Interface Alias: " + alias)
+    print("")
     print("Current Interface settings:")
     print("IPv4 Address: " + ipv4)
     print("IPv6 Address: " + ipv6)
     print("No. of peers: " + str(peercount))
-    title: str = "Editing P2MP Interface: " + intname
-    if alias != "":
-        title = title + "with Alias: " + alias
-    labels: list = ["Return to P2MP selection", "Edit clients", "De-/activate network"]
-    commands: list = [returnselection, (modifyp2mpclientsmenu, conf), (p2mpupdownmenu, interface)]
+    title: str = "Editing: " + intname
+    labels: list = ["Return to P2MP selection", "Edit clients", "Add client", "De-/activate network"]
+    commands: list = [returnmenu, (p2mppeerselectmenu, conf), (p2mpaddpeer, conf), (p2mpupdownmenu, interface)]
     makemenu(title, labels, commands)
 
 
 # The thing you might do after reading and before writing
-def modifyp2mpclientsmenu(conf):
-    title: str = "TBD Edit Clients on " + conf.interface
-    labels: list = ["Return to the previous menu"]
-    commands: list = [returnselection]
-    makemenu(title, labels, commands)
+def p2mppeerselectmenu(conf):
+    originalconf = copy.deepcopy(conf)
+    intname = conf.interface
+    alias = conf.alias
+    title: str = ""
+    labels: list = ["Return to previous menu"]
+    commands: list = [returnmenu]
+    print("")
+    print("Editing P2MP Interface: " + intname)
+    if alias != "":
+        print("Interface Alias: " + alias)
+    print("")
+    print("Active peer configs:")
+    for peer in conf.peers:
+        labels.append("\nAlias: " + peer.alias + "\nIPv4 : " + peer.ipv4 +
+                      "\nIPv6 : " + peer.ipv6 + "\n")
+        commands.append("returnselection")
+    selection: int = makemenu(title, labels, commands)
+    p2mppeermenu(conf, selection - 1)
+
+
+def p2mppeermenu(conf, peerindex):
+    while True:
+        peer = conf.peers[peerindex]
+        peer.printpeer()
+        title: str = ""
+        labels: list = ["Return to previous menu", "Edit Alias", "Edit IPv4", "Edit IPV6", "Edit Public Key"]
+        commands: list = [returnmenu, ("peer = peeredit", peer, "alias"), ("peer = peeredit", peer, "ipv4"),
+                          (peeredit, peer, "ipv6"), (peeredit, peer, "publickey")]
+        makemenu(title, labels, commands)
+
+
+def peeredit(peer, option: str):
+    print("Current value for " + option + " : " + peer.option)
+    newvalue = input("Enter new value or 0 to cancel: ")
+    if newvalue == "0":
+        return peer
+    else:
+        peer.option = newvalue
+        return peer
+
+
+def p2mpaddpeer(conf):
+    print("TBD")
 
 
 # Turn me on! Flip my switch!
@@ -213,7 +250,6 @@ def readconfig(filepath):
     file.close()
     linecount: int = 0
     intstartline: int = -1
-    intendline: int = -1
     peerstartlines: list = []
     for line in config:
         line = line.strip()
@@ -226,7 +262,7 @@ def readconfig(filepath):
         raise Exception("No valid interface config has been found in " + filepath)
     interfaceConfig = ConfigInterface(filepath)
     peerstartlines.append(linecount)
-    intendline = peerstartlines[0]
+    intendline: int = peerstartlines[0]
 
     # Parse Interface block, add to instanced object
     for line in config[intstartline:intendline]:
@@ -278,8 +314,13 @@ def invalidoption(tries: int, maxtries: int):
 
 
 # Menu leaver. Basically "There's the door."
-def returnselection():
+def returnmenu():
     return True
+
+
+# Menu returner. I'll take door number three, Bob.
+def returnselection(selection: int):
+    return selection
 
 
 # For when you don't throw a tantrum and want to thank the user.
@@ -309,8 +350,7 @@ class ConfigInterface:
 
 
 class ConfigPeer:
-    def __init__(self, interface: str, alias: str = "", publickey: str = "", ipv4: str = "", ipv6: str = ""):
-        self.interface = interface.split(".")[0].strip()
+    def __init__(self, alias: str = "", publickey: str = "", ipv4: str = "", ipv6: str = ""):
         self.alias: str = alias
         self.publickey: str = publickey
         self.ipv4: str = ipv4
@@ -319,6 +359,11 @@ class ConfigPeer:
     def printpeer(self):
         print("Peer: " + self.alias)
         print("IPv4: " + self.ipv4)
+        if self.ipv6 != "":
+            print("IPv6: " + self.ipv6)
+        else:
+            print("IPv6 not configured")
+        print("Public key: " + self.publickey)
 
 
 wgc()
